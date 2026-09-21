4d5d97f6d170d7caa62accd65b1342d794b36400
m-elias <eliaschief@gmail.com>
2026-09-21 11:39:17 -0500

    changed GPS2 baud to use same baud as RS232 (NMEA) output so that all 2630 NMEA bauds match

--- lib/aio_system/Version.h
+++ lib/aio_system/Version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 // AiO New Dawn firmware version
-#define FIRMWARE_VERSION "1.0.73-beta-rowsense-nudge-2630-nmea" // Merged in 4 cherry-picked commits from gs3-2630-nmea branch
+#define FIRMWARE_VERSION "1.0.73-beta-rowsense-nudge-2630-nmea-19200" // Indicates a beta release with rowsense, nudge, GS3 2630 support, and NMEA at 19200 baud
 
 // Teensy board type for OTA identification
 #define TEENSY_BOARD_TYPE "Teensy41"
